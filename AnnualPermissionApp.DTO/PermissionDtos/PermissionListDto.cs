d9b653e5d82903a7379e11dea5a58d0574d8a729
emreyuce90 <emreyuce90@hotmail.com>
2021-12-06 22:26:37 +0300

    Image

--- AnnualPermissionApp.DTO/PermissionDtos/PermissionListDto.cs
+++ AnnualPermissionApp.DTO/PermissionDtos/PermissionListDto.cs
@@ -4,7 +4,7 @@ namespace PermissionApp.AnnualPermissionApp.DTO.PermissionDtos
 {
     public class PermissionListDto
     {
-        
+        public int Id { get; set; }
         public int EmployeeId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
