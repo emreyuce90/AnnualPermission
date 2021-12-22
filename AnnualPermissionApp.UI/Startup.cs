7b16a69784cdf3eb2575f2b01e78217f0d31ab8a
Emre YÜCE <emreyuce90@hotmail.com>
2021-12-22 11:27:28 +0300

    Some changes

--- AnnualPermissionApp.UI/Startup.cs
+++ AnnualPermissionApp.UI/Startup.cs
@@ -26,12 +26,12 @@ namespace AnnualPermissionApp.UI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
-            app.UseExceptionHandler("/Error");
+            //app.UseExceptionHandler("/Error");
             app.UseRouting();
             
             app.UseEndpoints(endpoints =>
